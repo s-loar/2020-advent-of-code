f138f767135a7ccc48c926baececba32c808387b
Steven Loar <sjl7909@gmail.com>
2020-12-01 13:42:48 -0500

    Add code to solve day 1’s problem

---
+++ day-01/expense_report.rb
@@ -0,0 +1,53 @@
+class ExpenseReport
+  @expenses = []
+
+  def initialize
+    @expenses = File.readlines('./input.txt')
+  end
+
+  def write_expenses
+    @expenses.each do |expense|
+      puts expense
+    end
+  end
+
+  def two_number_solution
+    @expenses.each_with_index do |x, outer_index|
+
+      @expenses.each_with_index do |y, inner_index|
+        next if outer_index == inner_index
+
+        if (x.to_i + y.to_i) == 2020
+          puts "solution:"
+          puts "x: " + x
+          puts "y: " + y
+          puts "multiplied: " + (x.to_i * y.to_i).to_s
+          return
+        end
+      end
+    end
+    puts "No 2 expenses added to 2020"
+  end
+
+  def three_number_solution
+    @expenses.each_with_index do |x, x_index|
+
+      @expenses.each_with_index do |y, y_index|
+        next if x_index == y_index
+
+        @expenses.each_with_index do |z, z_index|
+          next if (y_index == z_index || x_index == z_index)
+          if (x.to_i + y.to_i + z.to_i) == 2020
+            puts "solution:"
+            puts "x: " + x
+            puts "y: " + y
+            puts "z: " + z
+            puts "multiplied: " + (x.to_i * y.to_i * z.to_i).to_s
+            return
+          end
+        end
+      end
+    end
+  end
+  puts "No 3 expenses added to 2020"
+end
---
+++ day-01/solution.rb
@@ -0,0 +1,9 @@
+require_relative 'expense_report'
+
+expense_report = ExpenseReport.new
+
+puts "2 numbers problem:"
+expense_report.two_number_solution
+
+puts "3 numbers problem:"
+expense_report.three_number_solution
