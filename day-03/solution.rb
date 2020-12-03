5cb7d352cdc2b06cea7c0d8497f77dec3ed9b275
Steven Loar <sjl7909@gmail.com>
2020-12-03 12:52:33 -0500

    Solution to second problem and some cleanup

--- day-03/solution.rb
+++ day-03/solution.rb
@@ -2,6 +2,14 @@ require_relative 'toboggan_route'
 
 toboggan_route = TobogganRoute.new
 
-# toboggan_route.write_map_rows
+toboggan_route.write_map_rows
 
 puts "First tree count: #{toboggan_route.tree_count(3,1)}"
+
+first = toboggan_route.tree_count(1,1)
+second = toboggan_route.tree_count(3,1)
+third = toboggan_route.tree_count(5,1)
+fourth = toboggan_route.tree_count(7,1)
+fifth = toboggan_route.tree_count(1,2)
+
+puts "Second tree product: #{ first * second * third * fourth * fifth }"
--- day-03/toboggan_route.rb
+++ day-03/toboggan_route.rb
@@ -3,14 +3,14 @@ class TobogganRoute
   @row_size = 0
 
   def initialize
-    @map_rows = File.readlines('./input.txt')
-    @map_rows = clean_map_rows
+    raw_map_rows = File.readlines('./input.txt')
+    @map_rows = clean_map_rows(raw_map_rows)
     @row_size = @map_rows[0].length
   end
 
   def write_map_rows
     @map_rows.each_with_index do |map_row, i|
-      p "Row #{i} #{map_row}"
+      puts "Row #{i} #{map_row}"
     end
   end
 
@@ -26,7 +26,6 @@ class TobogganRoute
     current_y = offset_y
 
     while current_y < @map_rows.size do
-      # p "Row #{current_y}, col #{current_x} is #{@map_rows[current_y][current_x]}"
       tree_count += 1 if @map_rows[current_y][current_x] == '#'
       current_y += offset_y
       current_x = (current_x + offset_x) % @row_size
@@ -35,9 +34,9 @@ class TobogganRoute
     tree_count
   end
 
-  def clean_map_rows
+  def clean_map_rows(raw_map_rows)
     new_map_rows = []
-    @map_rows.each do |map_row|
+    raw_map_rows.each do |map_row|
       if map_row[-1] == "\n"
         new_map_rows << map_row.chop
       else
