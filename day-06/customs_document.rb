f5d17b29952a3bea737000c5510513127b23f853
Steven Loar <sjl7909@gmail.com>
2020-12-07 15:12:15 -0500

    Solution to second problem, refactor and clean code.

--- day-06/customs_document.rb
+++ day-06/customs_document.rb
@@ -8,20 +8,17 @@ class CustomsDocument
 
   def write_customs_documents
     @documents.each_with_index do |document, i|
-      puts "document #{i.to_s}: #{document}"
+      puts "document #{i}: #{document}"
     end
   end
 
   def sum_of_group_yes
-    group_yes = 0
     total_yes = 0
     yes_counter = {}
 
     @documents.each do |document|
-      # puts "Document row: #{document}"
       if document.empty?
         puts "yes_counter: #{yes_counter.size}"
-        group_yes = 0
         total_yes += yes_counter.size
         yes_counter = {}
         next
@@ -40,7 +37,40 @@ class CustomsDocument
     total_yes
   end
 
+  def agreed_group_yes
+    total_yes = 0
+    yes_counter = Hash.new(0)
+    group_size = 0
+
+    @documents.each do |document|
+      if document.empty?
+        puts "yes_counter: #{yes_counter}"
+        total_yes += agreed_by_group_count(yes_counter, group_size)
+        yes_counter = Hash.new(0)
+        group_size = 0
+        next
+      end
+
+      group_size += 1
+      answers = document.split('').sort
+      answers.each do |question|
+        yes_counter[question] += 1
+      end
+    end
+
+    puts "yes_counter: #{yes_counter}"
+    total_yes += agreed_by_group_count(yes_counter, group_size)
+
+    total_yes
+  end
+
   private
+
+  def agreed_by_group_count(yes_hash, group_size)
+    agreed_answers = yes_hash.reject{|k,v|v != group_size}.keys.size
+    puts "group_size: #{group_size} | agreed_answers: #{agreed_answers}"
+    agreed_answers
+  end
 
   def clean(raw_data)
     clean_data = []
--- day-06/solution.rb
+++ day-06/solution.rb
@@ -5,3 +5,5 @@ customs_document = CustomsDocument.new
 customs_document.write_customs_documents
 
 puts "First Problem - the sum of 'yes' answers is #{customs_document.sum_of_group_yes}"
+
+puts "Second Problem - the sum of group agreed 'yes' answers is #{customs_document.agreed_group_yes}"
