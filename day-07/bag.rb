0bc5e675c29cbc9e4f6b804478bfebb33cbf04da
Steven Loar <sjl7909@gmail.com>
2020-12-08 13:04:12 -0500

    Solution to second problem for Day-07

--- day-07/bag.rb
+++ day-07/bag.rb
@@ -21,23 +21,39 @@ class Bag
 
   def bags_contained_in(bag_type)
     puts "#{bag_type} contains the following bags"
+    total_bag_count = contains(bag_type, 1)
+    puts "#{bag_type} contains #{total_bag_count} bags"
   end
 
   private
 
-  def contains(bag_type)
+  def contains(bag_type, bag_count)
+    total_bags = 0
     bag_type = bag_type[0...-1] if bag_type[-1] == 's'
     bags = @bag_rules.select{|key, value| key.include?(bag_type)}
-    puts "#{bag_type} directly contains #{bags.length} bag types"
     bags.each do |key,value|
       puts "#{bag_type}: rule #{key}: #{value}"
+      if value.include?('no other bags')
+        puts "0 inner bags for #{bag_type}"
+        return 0 if value.include?('no other bags')
+      end
+      inner_bags = value.split(', ')
+      puts "#{bag_type} directly contains #{inner_bags.length} bag types"
+      inner_bags.each do |inner_bag|
+        bag = inner_bag.split(' ')
+        number_of_inner_bag = bag[0].to_i * bag_count
+        inner_bag_name = bag[1..].join(' ')
+        puts "#{bag_type}: #{number_of_inner_bag} of #{inner_bag_name}"
+        total_bags += (bag[0].to_i * bag_count) + contains(inner_bag_name, number_of_inner_bag)
+      end
     end
+    puts "** Total Bags for (#{bag_type}): #{total_bags}"
+    total_bags
   end
 
   def containing(bag_type)
     key_bags = []
     bag_type = bag_type[0...-1] if bag_type[-1] == 's'
-
     bags = @bag_rules.select{|key, value| value.include?(bag_type)}
     puts "#{bag_type} is directly in #{bags.length} bags"
     bags.each do |key, value|
@@ -52,7 +68,7 @@ class Bag
     clean_hash = Hash.new
 
     raw_data.each do |row|
-      clean_data = row.gsub(/\R+/, '')
+      clean_data = row.gsub(/\R+/, '').gsub('.', '')
       tmp_array = clean_data.split('s contain ')
       clean_hash[tmp_array[0]] = tmp_array[1]
     end
--- day-07/solution.rb
+++ day-07/solution.rb
@@ -4,4 +4,6 @@ bag = Bag.new
 
 # bag.write_bag_rules
 
-bag.bags_containing('shiny gold bags')
+# bag.bags_containing('shiny gold bags')
+
+bag.bags_contained_in('shiny gold bags')
